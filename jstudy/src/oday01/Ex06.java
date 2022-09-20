8e73f5a8e79690718ca06219f60aa36b4fd7ac93
15B <15B@DESKTOP-CFPVGMR>
2022-09-20 18:52:56 +0900

    aasss

--- jstudy/src/oday01/Ex06.java
+++ jstudy/src/oday01/Ex06.java
@@ -12,9 +12,12 @@ public class Ex06 {
  */
 	public static void main(String[] args) {
 		
-		for(int i = 0 ; i < 5 ; i++) {
-			/* int no = " ***** "; */
-			System.out.println("*****");
+		for(int j = 0 ; j < 5 ; j++) {
+			for(int i = 0 ; i < 5 ; i++) {
+				/* int no = " ***** "; */
+				System.out.print("*"); // 출력기능만
+			}
+			System.out.println(); // 줄바꿈기능 추가
 		}
 	}
 
